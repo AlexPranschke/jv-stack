3bfc6e4dd1fb8b8693e834f7621c61f103660070
AlexPranschke <alexpranschke@gmail.com>
2026-05-21 22:41:40 +0200

    first

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,47 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+
+    private Node front = null;
+    private int size = 0;
+
+
+    public void push(T value) {
+        Node node = new Node(value);
+        node.prev = front;
+        front = node;
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (front == null) {
+            throw new EmptyStackException();
+        }
+        return front.value;
     }
 
     public T pop() {
-        return null;
+        if (front == null) {
+            throw new EmptyStackException();
+        }
+        Node temp = front;
+        front = front.prev;
+        size--;
+        return temp.value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private class Node {
+        private T value;
+        private Node prev;
+
+        public Node(T value) {
+            this.value = value;
+        }
     }
 }
